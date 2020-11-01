c9fdc4f786d1887ff4c10397cef04ad6b2e5e8d3
tsundere <73795265+r6league@users.noreply.github.com>
2020-11-01 19:05:40 +0000

    Create index.html

---
+++ index.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+   <head>
+    <title>PASH Leagues and Tournaments</title>
+    <style>
+      .img-container {
+        text-align: center;
+      }
+    </style>
+
+      <title>PASH Discord</title>
+      <meta http-equiv = "refresh" content = "2; url = https://discord.gg/ufd5uHMdYq" />
+   </head>
+   <body>
+    <div class="img-container"> <!-- Block parent element -->
+      <img src="https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fstackoverflow.com%2Fquestions%2F55960021%2Ftransparent-animated-gif-picture-show-above-another-picture&psig=AOvVaw2NDdzre_dOvQKS_QgEbQ1f&ust=1604343821574000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCICny9KE4uwCFQAAAAAdAAAAABAD" alt="Loading">
+    </div>
+   </body>
+</html>
